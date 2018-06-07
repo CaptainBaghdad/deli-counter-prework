b752380d55bf30ab00d695f57f09fdb3fda4df05
Jason Pitts <juniorjr083@gmail.com>
2018-06-07 01:59:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+
+def line(arr)
+ if  arr.length == 0 
+   puts "The line is currently empty."
+ else 
+   str = "The line is currently: "
+   arr.each_with_index{|e,i| str += "#{i+1}. #{e}"}
+   
+   
+ end 
+
+   
+end 
